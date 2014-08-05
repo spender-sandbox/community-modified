5a588c57a9dac873c2e9e54dc044f53ca32aa480
Brad Spengler <bspengler@accuvant.com>
2014-08-05 14:24:49 -0400

    Fix up registry regexes and update list of registry key infostealer locations from Fareit analysis.  Also log the entries we match.

--- modules/signatures/infostealer_ftp.py
+++ modules/signatures/infostealer_ftp.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2012 Claudio "nex" Guarnieri (@botherder)
+# Copyright (C) 2012-2014 Claudio "nex" Guarnieri (@botherder), Accuvant Inc. (bspengler@accuvant.com)
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -20,8 +20,8 @@ class FTPStealer(Signature):
     description = "Harvests credentials from local FTP client softwares"
     severity = 3
     categories = ["infostealer"]
-    authors = ["nex"]
-    minimum = "0.5"
+    authors = ["nex", "Accuvant"]
+    minimum = "1.0"
 
     def run(self):
         file_indicators = [
@@ -30,7 +30,8 @@ class FTPStealer(Signature):
             ".*\\\\FlashFXP\\\\.*\\\\Sites\.dat$",
             ".*\\\\FileZilla\\\\sitemanager\.xml$",
             ".*\\\\FileZilla\\\\recentservers\.xml$",
-            ".*\\\\VanDyke\\\\Config\\\\Sessions.*",
+            ".*\\\\VanDyke\\\\Config\\\\Sessions\\\\.*",
+            ".*\\\\Far Manager\\\\.*",
             ".*\\\\FTP Explorer\\\\.*"
             ".*\\\\SmartFTP\\\\.*",
             ".*\\\\TurboFTP\\\\.*",
@@ -38,21 +39,37 @@ class FTPStealer(Signature):
             ".*\\\\LeapFTP\\\\.*",
             ".*\\\\FTPGetter\\\\.*",
             ".*\\\\ALFTP\\\\.*",
-            ".*\\\\Ipswitch\\\\WS_FTP.*",
+            ".*\\\\Ipswitch\\\\WS_FTP\\\\.*",
         ]
         registry_indicators = [
-            ".*Software\\Far*\\Hosts$",
-            ".*Software\\Far*\\FTPHost$",
-            ".*Software\\Ghisler\\Windows Commander$",
-            ".*Software\\Ghisler\\Total Commander$",
-            ".*Software\\BPFTP\\$",
-            ".*Software\\BulletProof Software\BulletProof FTP Client\\$"
+            ".*\\\\Software\\\\Far.*\\\\Hosts$",
+            ".*\\\\Software\\\\Far.*\\\\FTPHost$",
+            ".*\\\\Software\\\\GlobalSCAPE\\\\CuteFTP.*",
+            ".*\\\\Software\\\\Ghisler\\\\Windows Commander.*",
+            ".*\\\\Software\\\\Ghisler\\\\Total Commander.*",
+            ".*\\\\Software\\\\BPFTP\\\\.*",
+            ".*\\\\Software\\\\FileZilla.*",
+            ".*\\\\Software\\\\TurboFTP.*",
+            ".*\\\\Software\\\\Sota\\\\FFFTP.*",
+            ".*\\\\Software\\\\FTPWare\\\\CoreFTP\\\\.*",
+            ".*\\\\Software\\\\FTP\\ Explorer\\\\.*",
+            ".*\\\\Software\\\\FTPClient\\\\.*",
+            ".*\\\\Software\\\\LinasFTP\\\\.*",
+            ".*\\\\Software\\\\Robo-FTP.*",
+            ".*\\\\Software\\\\MAS-Soft\\\\FTPInfo\\\\.*",
+            ".*\\\\Software\\\\SoftX\.org\\\\FTPClient\\\\.*",
+            ".*\\\\Software\\\\NCH\\ Software\\\\CoreFTP\\\\.*",
+            ".*\\\\Software\\\\BulletProof Software\\\\BulletProof FTP Client.*"
         ]
 
         for indicator in file_indicators:
-            if self.check_file(pattern=indicator, regex=True):
+            file_match = self.check_file(pattern=indicator, regex=True)
+            if file_match:
+                self.data.append({"file" : file_match })
                 return True
         for indicator in registry_indicators:
-            if self.check_key(pattern=indicator, regex=True):
+            key_match = self.check_key(pattern=indicator, regex=True)
+            if key_match:
+                self.data.append({"key" : key_match })
                 return True
         return False
