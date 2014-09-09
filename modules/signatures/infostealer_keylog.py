d24911fade5472f1adb66c025d3337c59b399aa0
Brad Spengler <bspengler@accuvant.com>
2014-09-09 14:15:53 -0400

    Add keylogger signature module

---
+++ modules/signatures/infostealer_keylog.py
@@ -0,0 +1,21 @@
+# Copyright (C) 2014 Accuvant, Inc. (bspengler@accuvant.com)
+# This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
+# See the file 'docs/LICENSE' for copying permission.
+from lib.cuckoo.common.abstracts import Signature
+
+class KeyLogger(Signature):
+    name = "infostealer_keylog"
+    description = "Sniffs keystrokes"
+    severity = 3
+    categories = ["infostealer"]
+    authors = ["Accuvant"]
+    minimum = "1.0"
+    evented = True
+
+    filter_apinames = set(["SetWindowsHookExA","SetWindowsHookExW"])
+
+    def on_call(self, call, process):
+        id = int(self.get_argument(call, "HookIdentifier"), 10)
+        # WH_KEYBOARD_LL
+        if id == 13:
+            return True
