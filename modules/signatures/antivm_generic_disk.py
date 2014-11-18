6e995915559a45cd00529ea107ade95a94a0c98a
Brad Spengler <bspengler@accuvant.com>
2014-11-18 13:32:48 -0500

    Fix the generic disk anti-vm signature since it depended on the string of integer representation of IoControlCode, add detection for another anti-vm method, and completely rewrite its  handle following, since if the first opened handle to the harddrive device doesn't have the expected operation performed on it, the signature would never be able to match

--- modules/signatures/antivm_generic_disk.py
+++ modules/signatures/antivm_generic_disk.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2012 Claudio "nex" Guarnieri (@botherder)
+# Copyright (C) 2012,2014 Claudio "nex" Guarnieri (@botherder), Accuvant, Inc. (bspengler@accuvant.com)
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -20,40 +20,48 @@ class DiskInformation(Signature):
     description = "Queries information on disks, possibly for anti-virtualization"
     severity = 3
     categories = ["anti-vm"]
-    authors = ["nex"]
-    minimum = "1.0"
+    authors = ["nex", "Accuvant"]
+    minimum = "1.2"
     evented = True
 
     def __init__(self, *args, **kwargs):
         Signature.__init__(self, *args, **kwargs)
-        self.lastprocess = None
+        self.lastprocess = 0
+        self.handles = dict()
 
-    filter_apinames = set(["NtCreateFile", "DeviceIoControl"])
+    filter_apinames = set(["NtCreateFile", "NtOpenFile", "NtClose", "DeviceIoControl", "NtDuplicateObject", "NtDeviceIoControlFile"])
 
     def on_call(self, call, process):
-        indicators = [
-            "scsi0",
-            "physicaldrive0"
-        ]
-
         ioctls = [
-            "2954240", # IOCTL_STORAGE_QUERY_PROPERTY
-            "458752", # IOCTL_DISK_GET_DRIVE_GEOMETRY
-            "315400" #IOCTL_SCSI_MINIPORT
+            0x2d1400, # IOCTL_STORAGE_QUERY_PROPERTY
+            0x70000,  # IOCTL_DISK_GET_DRIVE_GEOMETRY
+            0x4d008,  # IOCTL_SCSI_MINIPORT
+            0x7405c,  # IOCTL_DISK_GET_LENGTH_INFO
         ]
 
         if process is not self.lastprocess:
-            self.handle = None
+            self.handles = dict()
             self.lastprocess = process
 
-        if not self.handle:
-            if call["api"] == "NtCreateFile":
-                file_name = self.get_argument(call, "FileName")
-                for indicator in indicators:
-                    if indicator in file_name.lower():
-                        self.handle = self.get_argument(call, "FileHandle")
-        else:
+        if call["api"] == "NtDuplicateObject" and call["status"]:
+            srchandle = int(self.get_argument(call, "SourceHandle"), 16)
+            tgthandle = int(self.get_argument(call, "TargetHandle"), 16)
+            if srchandle in self.handles:
+                self.handles[tgthandle] = self.handles[srchandle]
+        elif call["api"] == "NtClose":
+            handle = int(self.get_argument(call, "Handle"), 16)
+            self.handles.pop(handle, None)
+        elif (call["api"] == "NtCreateFile" or call["api"] == "NtOpenFile") and call["status"]:
+            filename = self.get_argument(call, "FileName")
+            handle = int(self.get_argument(call, "FileHandle"), 16)
+            if filename and (filename.lower() == "\\??\\physicaldrive0" or filename.lower().startswith("\\device\\harddisk") or "scsi0" in filename.lower()):
+                if handle not in self.handles:
+                    self.handles[handle] = filename
+        elif call["api"] == "DeviceIoControl" or call["api"] == "NtDeviceIoControlFile":
+            ioctl = int(self.get_argument(call, "IoControlCode"), 16)
             if call["api"] == "DeviceIoControl":
-                if self.get_argument(call, "DeviceHandle") == self.handle:
-                    if str(self.get_argument(call, "IoControlCode")) in ioctls:
-                        return True
+                handle = int(self.get_argument(call, "DeviceHandle"), 16)
+            else:
+                handle = int(self.get_argument(call, "FileHandle"), 16) 
+            if handle in self.handles and ioctl in ioctls:
+                return True
