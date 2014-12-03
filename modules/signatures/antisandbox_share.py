8cce20d243fcb6022434fb7e03d268aca2730dbf
Brad Spengler <bspengler@accuvant.com>
2014-12-03 15:14:30 -0500

    Add two new signature modules, the first checks if the malware attempted to prevent cuckoo from obtaining dropped files by maintaining an open handle to a dropped file with FILE_SHARE_READ not specified -- a bug we've recently worked around in cuckoomon
    The second signature modules checks if the malware is trying to identify installed AV products based on installation paths

---
+++ modules/signatures/antisandbox_share.py
@@ -0,0 +1,47 @@
+# Copyright (C) 2014 Accuvant Inc. (bspengler@accuvant.com)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from lib.cuckoo.common.abstracts import Signature
+
+class AntiSandboxShare(Signature):
+    name = "antisandbox_share"
+    description = "Attempted to prevent Cuckoo from obtaining a dropped file"
+    severity = 3
+    categories = ["anti-sandbox"]
+    authors = ["Accuvant"]
+    minimum = "1.2"
+    evented = True
+
+    def __init__(self, *args, **kwargs):
+        Signature.__init__(self, *args, **kwargs)
+        self.unshared = set()
+
+    filter_apinames = set(["NtOpenFile", "NtCreateFile"])
+
+    def on_call(self, call, process):
+        share = int(self.get_argument(call, "ShareAccess"), 16)
+        # missing FILE_SHARE_READ
+        if (share & 1) == 0:
+            self.unshared.add(self.get_argument(call, "FileName"))
+    def on_complete(self):
+        found_unshared_drop = False
+        for unshare in self.unshared:
+            unsharedlower = unshare.lower()
+            for drop in self.results["dropped"]:
+                for path in drop["guest_paths"]:
+                    if path.lower() == unsharedlower:
+                        self.data.append({"binary" : path})
+                        found_unshared_drop = True
+        return found_unshared_drop
