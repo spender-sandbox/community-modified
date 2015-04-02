3cf04e8cd06a71e0efa400a131eb16e341afd2ec
Brad Spengler <bspengler@accuvant.com>
2015-04-02 15:07:49 -0400

    Add some tiny Dyre signature

---
+++ modules/signatures/dyre_apis.py
@@ -0,0 +1,36 @@
+# Copyright (C) 2015 Accuvant, Inc. (bspengler@accuvant.com)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from lib.cuckoo.common.abstracts import Signature
+
+class Dyre_APIs(Signature):
+    name = "dyre_behavior"
+    description = "Exhibits behavior characteristic of Dyre malware"
+    severity = 3
+    categories = ["banker", "trojan"]
+    families = ["dyre"]
+    authors = ["Accuvant"]
+    minimum = "1.2"
+    evented = True
+
+    def __init__(self, *args, **kwargs):
+        Signature.__init__(self, *args, **kwargs)
+
+    filter_apinames = set(["CryptHashData"])
+
+    def on_call(self, call, process):
+        buf = self.get_argument(call, "Buffer")
+        if buf == "qwererthwebfsdvjaf+\x00":
+            return True
