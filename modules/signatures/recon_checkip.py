1ee8a4892183fd3fe935b77e58d338b02d321f2a
Brad Spengler <bspengler@accuvant.com>
2015-05-22 13:43:20 -0400

    Fix up the recon_checkip signature to also work for STUN-based lookups.  Actually return all matches as well

--- modules/signatures/recon_checkip.py
+++ modules/signatures/recon_checkip.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2012 Claudio "nex" Guarnieri (@botherder)
+# Copyright (C) 2012,2015 Claudio "nex" Guarnieri (@botherder), Accuvant, Inc. (bspengler@accuvant.com)
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -20,11 +20,11 @@ class CheckIP(Signature):
     description = "Looks up the external IP address"
     severity = 2
     categories = ["recon"]
-    authors = ["nex"]
+    authors = ["nex", "Accuvant"]
     minimum = "0.5"
 
     def run(self):
-        indicators = [
+        dns_indicators = [
             "checkip.dyndns.com",
             "checkip.dyndns.org",
             "whatismyip.org",
@@ -40,11 +40,33 @@ class CheckIP(Signature):
             "myexternalip.com",
             "ip4.telize.com",
             "ip.xss.ru",
+
+            # public stun server list, from http://olegh.ftp.sh/public-stun.txt (could make this a feed I suppose)
+            # all servers not matching our generic stun[0-9]?.* pattern below
+            "iphone-stun.strato-iphone.de",
+            "numb.viagenie.ca",
+            "s1.taraba.net",
+            "s2.taraba.net",
+            "stunserver.org",
+        ]
+        ip_indicators = [
+            "23.21.150.121",
         ]
 
-        for indicator in indicators:
+        found_matches = False
+        for indicator in dns_indicators:
             if self.check_domain(pattern=indicator):
                 self.data.append({"domain" : indicator})
-                return True
+                found_matches = True
+        matches = self.check_domain(pattern="^stun[0-9]?\..*", regex=True, all=True)
+        if matches:
+            found_matches = True
+            for match in matches:
+                self.data.append({"domain" : match})
 
-        return False
+        for indicator in ip_indicators:
+            if self.check_ip(pattern=indicator):
+                self.data.append({"ip" : indicator})
+                found_matches = True
+
+        return found_matches
