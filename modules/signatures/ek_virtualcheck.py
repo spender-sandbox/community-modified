09d275a0ab2cf35c33b4fb9ff76bc530012da069
Brad Spengler <bspengler@accuvant.com>
2015-07-23 10:36:15 -0400

    Add an additional signature for sandbox checking through javascript, from Kevin Ross

---
+++ modules/signatures/ek_virtualcheck.py
@@ -0,0 +1,64 @@
+# Copyright (C) 2015 Kevin Ross
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+from lib.cuckoo.common.abstracts import Signature
+
+class Virtualcheck_JS(Signature):
+    name = "virtualcheck_js"
+    description = "Executes obfuscated JavaScript checking for sandbox or VM environment"
+    weight = 3
+    severity = 3
+    categories = ["exploit_kit", "evasion"]
+    authors = ["Kevin Ross"]
+    minimum = "1.3"
+    evented = True
+
+    def __init__(self, *args, **kwargs):
+        Signature.__init__(self, *args, **kwargs)
+
+    filter_categories = set(["browser"])
+    # backward compat
+    filter_apinames = set(["JsEval", "COleScript_Compile", "COleScript_ParseScriptText"])
+
+    def on_call(self, call, process):
+        indicators = [
+                "vmusbmouse",
+                "vmhgfs",
+                "vboxguest",
+                "vboxmouse",
+                "vmmouse",
+                "vm3dmp",
+                "prl_boot",
+                "prl_fs",
+                "prl_kmdd",
+                "prl_memdev",
+                "prl_mouf",
+                "prl_pv32",
+                "prl_sound",
+                "prl_prl_strg",
+                "prl_tg",
+                "prl_time"
+            ]
+
+        if call["api"] == "JsEval":
+            buf = self.get_argument(call, "Javascript")
+            for indicator in indicators:
+               if indicator in buf.lower():
+                return True
+        else:
+            buf = self.get_argument(call, "Script")
+            for indicator in indicators:
+               if indicator in buf.lower():
+                return True
