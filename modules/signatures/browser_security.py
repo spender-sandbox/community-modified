4fea3f0e2ed20e9f45d0264c9f2974281a6c5370
Brad Spengler <bspengler@accuvant.com>
2015-07-23 10:47:53 -0400

    Add another registry key, from Kevin Ross

--- modules/signatures/browser_security.py
+++ modules/signatures/browser_security.py
@@ -34,6 +34,7 @@ class BrowserSecurity(Signature):
         ".*\\\\SOFTWARE\\\\(Wow6432Node\\\\)?Microsoft\\\\Windows\\\\CurrentVersion\\\\Internet\\ Settings\\\\ZoneMap\\\\IEHarden$",
         ".*\\\\SOFTWARE\\\\(Wow6432Node\\\\)?Microsoft\\\\Windows\\\\CurrentVersion\\\\Internet\\ Settings\\\\CertificateRevocation$",
         ".*\\\\SOFTWARE\\\\(Wow6432Node\\\\)?Microsoft\\\\Internet\\ Explorer\\\\Main\\\\NoUpdateCheck$",
+        ".*\\\\SOFTWARE\\\\(Wow6432Node\\\\)?Microsoft\\\\Internet\\ Explorer\\\\Security\\\\.*",
         ]
 
         for indicator in reg_indicators:
