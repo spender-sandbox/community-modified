859e7f451041b4f11b671c20111e98a803df541d
Brad Spengler <bspengler@accuvant.com>
2015-07-23 16:38:30 -0400

    Add a clickfraud signature based on an internal analysis of Poweliks 1.8

---
+++ modules/signatures/clickfraud_volume.py
@@ -0,0 +1,27 @@
+# Copyright (C) 2015 Accuvant, Inc. (bspengler@accuvant.com)
+# This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
+# See the file 'docs/LICENSE' for copying permission.
+
+from lib.cuckoo.common.abstracts import Signature
+
+class ClickfraudVolume(Signature):
+    name = "clickfraud_volume"
+    description = "Attempts to disable browser navigation sounds, indicative of click fraud"
+    severity = 3
+    categories = ["clickfraud"]
+    authors = ["Accuvant"]
+    minimum = "1.3"
+    evented = True
+
+    def __init__(self, *args, **kwargs):
+        Signature.__init__(self, *args, **kwargs)
+
+    filter_apinames = set(["CoInternetSetFeatureEnabled"])
+
+    def on_call(self, call, process):
+        entry = int(self.get_argument(call, "FeatureEntry"), 10)
+        enable = int(self.get_argument(call, "Enabled"), 10)
+
+        # FEATURE_DISABLE_NAVIGATION_SOUNDS
+        if entry == 21 and enable:
+            return True
