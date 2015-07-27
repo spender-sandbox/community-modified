3ee589df43a17d6e379a7462ef3c20d162184f1b
Brad Spengler <bspengler@accuvant.com>
2015-07-27 16:53:40 -0400

    Update signature based on Cryptowall analysis

--- modules/signatures/modifies_seccenter.py
+++ modules/signatures/modifies_seccenter.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2015 Kevin Ross
+# Copyright (C) 2015 Kevin Ross, Accuvant, Inc. (bspengler@accuvant.com)
 # This file is part of Cuckoo Sandbox - http://www.cuckoosandbox.org
 # See the file 'docs/LICENSE' for copying permission.
 
@@ -9,11 +9,16 @@ class ModifySecurityCenterWarnings(Signature):
     description = "Attempts to modify or disable Security Center warnings"
     severity = 3
     categories = ["stealth"]
-    authors = ["Kevin Ross"]
+    authors = ["Kevin Ross", "Accuvant"]
     minimum = "1.2"
 
     def run(self):
-        if self.check_write_key(pattern=".*\\\\SOFTWARE\\\\(Wow6432Node\\\\)?Microsoft\\\\Security\\ Center\\\\.*", regex=True):
-            return True
+        indicators = [
+            ".*\\\\SOFTWARE\\\\(Wow6432Node\\\\)?Microsoft\\\\Security\\ Center\\\\.*",
+            ".*\\\\SOFTWARE\\\\Microsoft\\\\Windows\\\\CurrentVersion\\\\explorer\\\\ShellServiceObjects\\\\{FD6905CE-952F-41F1-9A6F-135D9C6622CC}$",
+        ]                                                                                                   
+        for indicator in indicators:
+            if self.check_write_key(pattern=indicator, regex=True):
+                return True
 
         return False
