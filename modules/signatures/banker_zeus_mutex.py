8398d4dcbf3932f96438bbef2e9aa64a17e6eb6a
Brad Spengler <bspengler@accuvant.com>
2015-09-08 11:03:54 -0400

    Required version should be 1.2

--- modules/signatures/banker_zeus_mutex.py
+++ modules/signatures/banker_zeus_mutex.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2014 Robby Zeitfuchs (@robbyFux)
+﻿# Copyright (C) 2014 Robby Zeitfuchs (@robbyFux)
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -22,7 +22,7 @@ class ZeusMutexes(Signature):
     categories = ["banker"]
     families = ["zeus"]
     authors = ["Robby Zeitfuchs", "KillerInstinct"]
-    minimum = "1.1"
+    minimum = "1.2"
     references = ["https://malwr.com/analysis/NmNhODg5ZWRkYjc0NDY0M2I3YTJhNDRlM2FlOTZiMjA/#summary_mutexes",
                   "https://malwr.com/analysis/MmMwNDJlMTI0MTNkNGFjNmE0OGY3Y2I5MjhiMGI1NzI/#summary_mutexes",
                   "https://malwr.com/analysis/MzY5ZTM2NzZhMzI3NDY2YjgzMjJiODFkODZkYzIwYmQ/#summary_mutexes",
