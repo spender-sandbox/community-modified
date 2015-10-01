138499f58e4cdaa2d667aaff8e101da3b1f64084
kevross33 <kevross33@gmail.com>
2015-10-01 09:25:15 +0100

    simplify signature
    Make the signature more redundant to potential executions using abbreviations by focusing on only key parts of the command.

--- modules/signatures/powershell_command.py
+++ modules/signatures/powershell_command.py
@@ -17,53 +17,53 @@ class PowershellCommand(Signature):
     def on_call(self, call, process):
         if call["api"] == "CreateProcessInternalW":
             cmdline = self.get_argument(call, "CommandLine").lower()
-            if "powershell.exe" in cmdline and "-ep bypass" in cmdline or "-executionpolicy bypass" in cmdline:
+            if "powershell.exe" in cmdline and "bypass" in cmdline:
                 self.data.append({"execution_policy" : "Attempts to bypass execution policy"})
                 self.severity = 3
                 self.weight += 1
         elif call["api"] == "ShellExecuteExW":
             filepath = self.get_argument(call, "FilePath").lower()
             params = self.get_argument(call, "Parameters").lower()
-            if "powershell.exe" in filepath and "-ep bypass" in params or "-executionpolicy bypass" in params:
+            if "powershell.exe" in filepath and "bypass" in params:
                 self.data.append({"execution_policy" : "Attempts to bypass execution policy"})
                 self.severity = 3
                 self.weight += 1
 
         if call["api"] == "CreateProcessInternalW":
             cmdline = self.get_argument(call, "CommandLine").lower()
-            if "powershell.exe" in cmdline and "-nop" in cmdline or "-noprofile" in cmdline:
+            if "powershell.exe" in cmdline and "-nop" in cmdline:
                 self.data.append({"user_profile" : "Does not load current user profile"})
                 self.severity = 3
                 self.weight += 1
         elif call["api"] == "ShellExecuteExW":
             filepath = self.get_argument(call, "FilePath").lower()
             params = self.get_argument(call, "Parameters").lower()
-            if "powershell.exe" in filepath and "-nop" in params or "-noprofile" in params:
+            if "powershell.exe" in filepath and "-nop" in params:
                 self.data.append({"user_profile" : "Does not load current user profile"})
                 self.severity = 3
                 self.weight += 1
 
         if call["api"] == "CreateProcessInternalW":
             cmdline = self.get_argument(call, "CommandLine").lower()
-            if "powershell.exe" in cmdline and "-w hidden" in cmdline "-windowstyle hidden" in cmdline:
+            if "powershell.exe" in cmdline and "-w" in cmdline and "hidden" in cmdline:
                 self.data.append({"hidden_window" : "Attempts to execute command with a hidden window"})
                 self.weight += 1
         elif call["api"] == "ShellExecuteExW":
             filepath = self.get_argument(call, "FilePath").lower()
             params = self.get_argument(call, "Parameters").lower()
-            if "powershell.exe" in filepath and "-w hidden" in params or "-windowstyle hidden" in params:
+            if "powershell.exe" in filepath and "-w" in params and "hidden" in params:
                 self.data.append({"hidden_window" : "Attempts to execute command with a hidden window"})
                 self.weight += 1
 
         if call["api"] == "CreateProcessInternalW":
             cmdline = self.get_argument(call, "CommandLine").lower()
-            if "powershell.exe" in cmdline and "-enc" in cmdline or "-encodedcommand" in cmdline:
+            if "powershell.exe" in cmdline and "-enc" in cmdline:
                 self.data.append({"b64_encoded" : "Uses a Base64 encoded command value"})
                 self.weight += 1
         elif call["api"] == "ShellExecuteExW":
             filepath = self.get_argument(call, "FilePath").lower()
             params = self.get_argument(call, "Parameters").lower()
-            if "powershell.exe" in filepath and "-enc" in params or "-encodedcommand" in params:
+            if "powershell.exe" in filepath and "-enc" in params:
                 self.data.append({"b64_encoded" : "Uses a Base64 encoded command value"})
                 self.weight += 1
 
