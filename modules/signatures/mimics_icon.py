a0d109aa256377013c50c3d710decc49d2fc0aba
Brad Spengler <bspengler@accuvant.com>
2015-11-11 08:32:01 -0500

    Manually merge https://github.com/KillerInstinct/community-modified/commit/8bdb5cf74cb3fa3aa3002a226c51faf616441ca2

--- modules/signatures/mimics_icon.py
+++ modules/signatures/mimics_icon.py
@@ -15,18 +15,24 @@ class MimicsIcon(Signature):
     def run(self):
         # Alphanumerica hash list by category
         badhashes = [
-            # newer word doc icon
-            "ec7e6f5458456dddb2d826bf1b8b03a2",
-            # PDF icon
             "059dcdf32e800b5f2fe2aea2d5f045d8",
             "2c45339aea71418c49248aa88ffb2378",
+            # Office 365 Application Logo
+            "4e623298caf36bc642543408fee2fd10",
+            # Adobe PDF icon
             "6890c8a40c2eb5ff973159eca0428d6e",
             "6b0de9fbca602a637b0580b879904d61",
             "9334967a316ffffd255aaf9224a7da5e",
             "b686a61a6fbd20073faf430128597795",
+            # Word 2007+ Application Logo
+            "bad3afc82412906dc308db9d96f95e88",
             "d05e789cb57b150c4315acd350fce088",
             "db2d2c31f0c651217a0ef11aaf8c7796",
             "e52d1e9d64fd9535bf10f6da1091df9d",
+            # Word 2007+
+            "ec7e6f5458456dddb2d826bf1b8b03a2",
+            # Fake PDF icon
+            "f042192565667b350a5056af6ce01d5c",
             "f25d6693364cba910762c7e1d5149c21",
         ]
 
