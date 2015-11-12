f2189ee82f19a8af92831739577b6b7f9b61bf4a
Brad Spengler <bspengler@accuvant.com>
2015-11-12 13:04:32 -0500

    Fix a FP

--- modules/signatures/stealth_timelimit.py
+++ modules/signatures/stealth_timelimit.py
@@ -38,8 +38,9 @@ class StealthTimeout(Signature):
             if handle == "0xffffffff" or handle == "0x00000000":
                 self.exitidx = self.curidx
                 if self.systimeidx and self.exitidx and self.systimeidx > (self.exitidx - 10):
-                    self.data.append({"process" : process["process_name"] + ", PID " + str(process["process_id"])})
-                    return True
+                    if process["module_path"].lower() != "c:\\windows\\system32\\attrib.exe":
+                        self.data.append({"process" : process["process_name"] + ", PID " + str(process["process_id"])})
+                        return True
 
         return None
 
