b4c0e420c27d057561a54e82da31e007ff1df35a
kevross33 <kevross33@gmail.com>
2016-01-04 23:32:47 +0000

    Create specific signature for Radamant ransomware

---
+++ modules/signatures/family_radamant.py
@@ -0,0 +1,30 @@
+from lib.cuckoo.common.abstracts import Signature
+
+class FamilyRadamant(Signature):
+    name = "family_radamant"
+    description = "Exhibits behavior characteristics of Radamant ransomware"
+    severity = 3
+    families = ["radamant"]
+    categories = ["ransomware"]
+    authors = ["Kevin Ross"]
+    minimum = "1.2"
+
+    def run(self):
+        mutexes = [
+            "Radamant_v.*",
+            ".*radamantv.*",
+        ]
+
+        for mutexes in mutexes:
+            if self.check_mutex(pattern=mutexes, regex=True):
+                return True
+
+        # Check for creation of Autorun
+        if self.check_write_key(pattern=".*\\\\SOFTWARE\\\\(Wow6432Node\\\\)?Microsoft\\\\Windows\\\\CurrentVersion\\\\Run\\\\svchost$", regex=True) and self.check_write_file(pattern=".*\\\\Windows\\\\dirextx.exe$", regex=True):
+            return True
+
+        # Check for creation of ransom message file
+        if self.check_write_file(pattern=".*\\\\YOUR_FILES.url$", regex=True):
+            return True
+
+        return False
