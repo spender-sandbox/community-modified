3b94c7222801d0e1c6cf0c9e2dc0aa02f10e5e15
KillerInstinct <KillerInstinct@users.noreply.github.com>
2016-02-12 09:18:16 -0500

    Fix copyright due to c/p an old version

--- modules/signatures/dridex_apis.py
+++ modules/signatures/dridex_apis.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2015 KillerInstinct
+# Copyright (C) 2015-2016 KillerInstinct
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
