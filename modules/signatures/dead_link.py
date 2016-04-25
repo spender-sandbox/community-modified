82d1e10da936899970614f58e416dfb53dae9f2c
Brad Spengler <spender+sandbox@gmail.com>
2016-04-25 11:15:59 -0400

    Fix dead link signature

--- modules/signatures/dead_link.py
+++ modules/signatures/dead_link.py
@@ -18,12 +18,19 @@ class DeadLink(Signature):
         Signature.__init__(self, *args, **kwargs)
         self.appnames = []
 
-    filter_apinames = set(["CreateProcessInternalW"])
+    filter_apinames = set(["CreateProcessInternalW", "ShellExecuteExW"])
 
     def on_call(self, call, process):
-        appname = self.get_argument(call, "ApplicationName").lower()
-        if appname not in self.appnames:
-            self.appnames.append(appname)
+        if call["api"] == "CreateProcessInternalW":
+            appname = self.get_argument(call, "ApplicationName").lower()
+            if appname == "":
+                appname = self.get_argument(call, "CommandLine").lower()
+            if appname not in self.appnames:
+                self.appnames.append(appname)
+        elif call["api"] == "ShellExecuteExW":
+            appname = self.get_argument(call, "FilePath").lower()
+            if appname not in self.appnames:
+                self.appnames.append(appname)
 
     def on_complete(self):
         if "dropped" in self.results:
