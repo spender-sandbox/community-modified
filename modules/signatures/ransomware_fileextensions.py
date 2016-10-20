8ea7f53cd333b831c8d83dd5ffb347ae078091be
kevross33 <kevross33@gmail.com>
2016-10-20 14:55:36 +0100

    Update ransomware file extensions & add family
    - Update to include families 
    - Removed TeslaCrypt stuff as outside of research it is gone & it was incomplete anyway.

--- modules/signatures/ransomware_fileextensions.py
+++ modules/signatures/ransomware_fileextensions.py
@@ -4,63 +4,69 @@ class RansomwareExtensions(Signature):
     name = "ransomware_extensions"
     description = "Appends known ransomware file extensions to files that have been encrypted"
     severity = 3
+    families = []
     categories = ["ransomware"]
     authors = ["Kevin Ross"]
     minimum = "1.2"
 
     def run(self):
         indicators = [
-            ".*\.aaa$",
-            ".*\.abc$",
-            ".*\.ccc$",
-            ".*\.ecc$",
-            ".*\.exx$",
-            ".*\.ezz$",
-            ".*\.vvv$",
-            ".*\.rdm$",
-            ".*\.rrk$",
-            ".*\.toxcrypt$",
-            ".*\.vault$",
-            ".*\.hydracrypt_ID_[a-z0-9]{8}$",
-            ".*\.hydracrypttmp_ID_[a-z0-9]{8}$",
-            ".*\.micro$",
-            ".*\.locky$",
-            ".*\.xtbl$",
-            ".*\.crypt$",
-            ".*\.locked$",
-            ".*\.cerber$",
-            ".*\.cerber2$",
-            ".*\.cerber3$",
-            ".*\.encrypt$",
-            ".*\.R5A$",
-            ".*\.R4A$",
-            ".*\.herbst$",
-            ".*\.CrySiS$",
-            ".*\.bart\.zip$",
-            ".*\.zepto$",
-            ".*\.wflx$",
-            ".*\.id_[^\/]*\.scl$",
-            ".*\.razy$",
-            ".*\.Venus(f|p)$",
-            ".*\.crypt$",
-            ".*\.crypz$",
-            ".*\.cryp1$",
-            ".*\.[0-9A-F]{32}\.[0-9A-F]{5}$",
-            ".*\.fs0ciety",
-            ".*\.cry",
-            ".*\.locked",
-            ".*\.locklock",
-            ".*\.fantom",
-            ".*_nullbyte",
-            ".*\.purge$",
-            ".*\.domino",
-            ".*\.odin",
-            ".*\.encrypted",
+            (".*\.toxcrypt$", ["ToxCrypt"]),
+            (".*\.hydracrypt_ID_[a-z0-9]{8}$", ["HydraCrypt"]),
+            (".*\.hydracrypttmp_ID_[a-z0-9]{8}$", ["HydraCrypt"]),
+            (".*\.locked$", ["Locked"]),
+            (".*\.cerber$", ["Cerber"]),
+            (".*\.cerber2$", ["Cerber"]),
+            (".*\.cerber3$", ["Cerber"]),
+            (".*\.encrypt$", ["multi-family"]),
+            (".*\.R5A$", ["7ev3n"]),
+            (".*\.R4A$", ["7ev3n"]),
+            (".*\.herbst$", ["Herbst"]),
+            (".*\.CrySiS$", ["Crysis"]),
+            (".*\.bart\.zip$", ["Bart"]),
+            (".*\.crypt$", ["CryptXXX"]),
+            (".*\.crypz$", ["CryptXXX"]),
+            (".*\.cryp1$", ["CryptXXX"]),
+            (".*\.[0-9A-F]{32}\.[0-9A-F]{5}$", ["CryptXXX"]),
+            (".*\.id_[^\/]*\.scl$", ["CryptFile2"]),
+            (".*\.razy$", ["Razy"]),
+            (".*\.Venus(f|p)$", ["VenusLocker"]),
+            (".*\.fs0ciety$", ["Fsociety"]),
+            (".*\.cry$", ["CryLocker"]),
+            (".*\.locklock$", ["LockLock"]),
+            (".*\.fantom$", ["Fantom"]),
+            (".*_nullbyte$", ["Nullbyte"]),
+            (".*\.purge$", ["Globe"]),
+            (".*\.globe$", ["Globe"]),
+            (".*\.raid10$", ["Globe"]),
+            (".*\.domino$", ["Domino"]),
+            (".*\.locky$", ["Locky"]),
+            (".*\.wflx$", ["Locky"]),
+            (".*\.zepto$", ["Locky"]),
+            (".*\.odin$", ["Locky"]),
+            (".*\.locked$", ["multi-family"]),
+            (".*\.encrypted$", ["multi-family"]),
+            (".*dxxd$", ["DXXD"]),
+            (".*\.~HL[A-Z0-9]{5}$", ["HadesLocker"]),
+            (".*\.exotic$", ["Exotic"]),
+            (".*\.k0stya$", ["Kostya"]),
+            (".*\.1txt$", ["Enigma"]),
+            (".*\.0x5bm$", ["Nuke"]),
+            (".*\.nuclear55$", ["Nuke"]),
+            (".*\.comrade$", ["Comrade-Circle"]),
+            (".*\.rip$", ["KillerLocker"]),
         ]
 
         for indicator in indicators:
-            results = self.check_write_file(pattern=indicator, regex=True, all=True)
+            results = self.check_write_file(pattern=indicator[0], regex=True, all=True)
             if results and len(results) > 15:
+                if indicator[1]:
+                    self.families = indicator[1]
+                    self.description = (
+                        "Appends known %s ransomware file extensions to "
+                        "files that have been encrypted" %
+                        "/".join(indicator[1])
+                    )
                 return True
 
         return False
